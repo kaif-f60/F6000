561ece61c2a8cce5a121d12723b340bf2025b199
kaif-f60 <kaif@pentagoat.com>
2025-04-18 17:37:43 +0530

    dxwexw

--- main2.js
+++ main2.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 let re = "hello world";
 let rw = "hello world";
 
 console.log(re === rw); // true
-=======
 let obj1 = {
-    name;"john",
+    name:"john",
     age: 30,
     city: "New York",
     }
@@ -15,4 +13,3 @@ let obj2 = {
     age: 40,
     city: "New York",
     }
->>>>>>> a78c21f809085c019f2923309f0cf0b0f3b1e7d4
