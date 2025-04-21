b4cf85d09a6cd39650d8116a30b128a12a016537
kaif-f60 <kaif@pentagoat.com>
2025-04-21 16:51:23 +0530

    start learning backend

---
+++ index.js
@@ -0,0 +1,32 @@
+express = require('express')
+const app  = express()
+
+
+app.listen(3000,()=>{
+   console.log('Succes Connect');
+})
+
+app.get('/',(req, res)=>{
+   res.send("<h1>Welcome to Homepage");
+})
+app.get('/about',(req, res)=>{
+    res.send("<h1>Welcome to About page");
+ })
+ app.get('/Gallery',(req, res)=>{
+    res.send("<h1>Welcome to Gallery page");
+ })
+ app.get('/Quality',(req, res)=>{
+    res.send("<h1>Welcome to Contact page");
+ })
+ app.get('/user/:userid-:bookid',(req, res)=>{
+    // res.send(req.params.userid);
+    // res.send(req.params.bookid);
+    res.send(req.params);
+ })
+
+
+ app.get('/search',(req, res)=>{
+    // res.send(req.params.userid);
+    // res.send(req.params.bookid);
+    res.send(req.query);
+ })
--- main.js
+++
@@ -1,7 +0,0 @@
-const a = 90;
-const b = 30;
-const c = a*b;
-
-console.log(c)
-console.log(c+" "+"hello")
-
