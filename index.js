d0dd736bc1a7f47a231eba85b45c89a91ba64e72
kaif-f60 <kaif@pentagoat.com>
2025-04-21 18:17:54 +0530

    Learn some methods of response

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ app.listen(3000,()=>{
 })
 
 app.get('/',(req, res)=>{
-   res.send("<h1>Welcome to Homepage");
+
+    const childs =[{name:"kailash", age:21},{name:"kailash1", age:25}]
+   res.json(
+    childs
+   );
 })
+
+
+
+
 app.get('/about',(req, res)=>{
     res.send("<h1>Welcome to About page");
  })
@@ -31,9 +39,41 @@ app.get('/about',(req, res)=>{
 //     res.send(req.query);
 //  })
 
- 
+//  Query Parameter
  app.get('/search',(req, res)=>{
     const name =  req.query.name
     const age =  req.query.age
     res.send(`search result for name : Name : ${name}, Age:${age} `);
  })
+
+// redirect Response
+
+// redirect method
+app.get('/abc',(req,res)=>{
+    res.redirect("https:pipex.ai")
+     })
+// redirect with status code
+     app.get('/abc1',(req,res)=>{
+        res.redirect(301, "https:pipex.ai")
+         })
+    
+// redirect with previous page
+app.get('/abc2',(req,res)=>{
+res.redirect("..")
+ })
+
+
+app.get('/abc3',(req,res)=>{
+res.jsonp({name: 'Kailesh', age:25})
+})
+
+// method to view html
+
+
+app.set('view engine', 'ejs')
+app.get('/abc4',(req,res)=>{
+    res.render('user')
+})
+    
+    
+    
